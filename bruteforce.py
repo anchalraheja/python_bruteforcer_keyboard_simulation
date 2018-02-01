c79dffe5304a62825e7ba6806dd52a762452b84b
Anchal Raheja <anchalpranav@gmail.com>
2018-02-01 15:16:34 -0600

    Update bruteforce.py

--- bruteforce.py
+++ bruteforce.py
@@ -6,6 +6,6 @@ time.sleep(1.5)
 
 for x in range(0000,9999):
 	y = str(x)
-	typewrite(y) .    #simulates keyboard input
+	typewrite(y)     #simulates keyboard input
 	hotkey('enter')   #simulates "enter" key 
 
